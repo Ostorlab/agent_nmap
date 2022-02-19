613475b15db915847d3baa63025e220a7c1565e7
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-02-20 00:58:16 +0100

    Wrapper refactoring.

--- agent/nmap.py
+++ agent/nmap.py
@@ -1,54 +1,81 @@
 """Wrapper for Nmap Security Scanner."""
 
 from typing import Any, Dict, List
+import dataclasses
+import ipaddress
+import logging
 import subprocess
 import json
 
 import xmltodict
 
+logger = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class NmapOptions:
+    """Storing the options of the nmap scan."""
+    dns_resolution: bool = True
+    dns_servers: List = None
+    ports: str = None
+    timing_template: int = 3
+
+    def get_command_options(self) -> List[str]:
+        """Returns a list of nmap options."""
+        command_options = []
+        if self.dns_resolution is True:
+            command_options.append('-R')
+            if self.dns_servers:
+                dns_servers = ','.join([str(dns) for dns in self.dns_servers])
+                command_options.append(f'--dns-servers {dns_servers}')
+        else:
+            command_options.append('-n')
+
+        ports = f'-p {self.ports}' * (self.ports is not None)
+        command_options.append(ports)
+
+        if self.timing_template > 5 or self.timing_template < 0:
+            logger.warning('The timing template should be between 0 and 5. The scan will use the default T3.')
+        else:
+            command_options.append('-T' + str(self.timing_template))
+
+        return command_options
+
 
 class NmapWrapper:
     """Wrapper class for the Nmap Security Scanner."""
 
-    def __init__(
-            self,
-            hosts: str = '127.0.0.1',
-            mask: str = '32',
-            ip_version: int = 4,
-            ports: str = '22,8080',
-            arguments: str = '-sV'
-        ) -> None:
+    def __init__(self, nmap_options: NmapOptions) -> None:
         """Constructs all the necessary attributes for the object.
 
         Args:
-            hosts: which hosts do you want to scan.
-            mask: mask to be used in the scan.
-            ip_version: ipv4 or ipv6.
-            ports: ports to be scanned.
-            arguments: arguments of the nmap command.
+            nmap_options: options of the nmap scan.
         """
-        self.hosts = hosts
-        self.ip_version = ip_version
-        self.mask = mask
-        self.ports = ports
-        self.arguments = arguments + f' -{self.ip_version}' * (self.ip_version==6)
-        self.command = self._construct_command()
+        self.nmap_options = nmap_options
 
-    def _construct_command(self) -> List[str]:
+    def _construct_command(self, host: str, mask: str) -> List[str]:
         """
         Construct the Nmap command to be run.
+
+        Args:
+            hosts: which hosts to be scanned.
+            mask: mask to be used in the scan.
 
         Returns:
             list of the arguments that will be used to run the scan process.
         """
-        ports = f'-p {self.ports}' * (self.ports is not None)
-        hosts_and_mask = self.hosts + '/' + self.mask
+        command_options = self.nmap_options.get_command_options()
+        ip_version = ipaddress.ip_address(host).version
+        ip_version_option = f' -{ip_version} ' * (ip_version == 6)
+        hosts_and_mask = ip_version_option + host + f'/{mask}' * (mask != '')
+
         command = ['nmap',
-                   self.arguments,
+                   '-sV',
+                   *command_options,
                    '--script=banner',
-                   '-oX -',
-                   hosts_and_mask,
-                   ports]
+                   '-oX',
+                   '-',
+                   hosts_and_mask]
         return command
 
     def _parse_output_to_json(self, xml_output: str) -> Dict[str, Any]:
@@ -65,14 +92,19 @@ class NmapWrapper:
         json_output = json.loads(json_output)
         return json_output
 
-    def scan(self) -> Dict[str, Any]:
+    def scan(self, hosts: str, mask: str = '') -> Dict[str, Any]:
         """Run the scan with nmap.
+
+        Args:
+            hosts: which hosts to be scanned.
+            mask: mask to be used in the scan.
 
         Returns:
             result of the scan.
         """
-        with subprocess.Popen(self.command, stdout=subprocess.PIPE) as process:
+        command = self._construct_command(hosts, mask)
+        with subprocess.Popen(command, stdout=subprocess.PIPE) as process:
             xml_output = process.communicate()[0]
-            xml_output= xml_output.decode(encoding='utf-8')
+            xml_output = xml_output.decode(encoding='utf-8')
             scan_results = self._parse_output_to_json(xml_output)
             return scan_results
