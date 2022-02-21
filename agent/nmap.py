3ca69306d88fa26374b2f0f8cfb6f2ab1fc1a2a1
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-02-21 18:22:34 +0100

    Add docstring, and refactor.

--- agent/nmap.py
+++ agent/nmap.py
@@ -5,11 +5,20 @@ import dataclasses
 import ipaddress
 import logging
 import subprocess
-import json
+import enum
+
 
 import xmltodict
 
 logger = logging.getLogger(__name__)
+
+
+class NmapTimingTemplate(enum.Enum):
+    T0 = '-T0'
+    T1 = '-T1'
+    T2 = '-T2'
+    T3 = '-T3'
+    T4 = '-T4'
 
 
 @dataclasses.dataclass
@@ -18,16 +27,18 @@ class NmapOptions:
     dns_resolution: bool = True
     dns_servers: List[str] = None
     ports: Optional[str] = None
-    timing_template: int = 3
-    enable_version: bool = True
+    timing_template: NmapTimingTemplate = NmapTimingTemplate.T3
+    enable_version_detection: bool = True
     command_options: List[str] = dataclasses.field(default_factory=list)
 
-    def _get_version_option(self):
-        if self.enable_version is True:
-            self.command_options.append('-V')
+    def _append_enable_version_detection_option(self):
+        """Appends the  option to the list of nmap options."""
+        if self.enable_version_detection is True:
+            self.command_options.append('-sV')
             self.command_options.append('--script=banner')
 
-    def _get_dns_resolution_option(self):
+    def _append_dns_resolution_option(self):
+        """Appends the dns resolution option to the list of nmap options."""
         if self.dns_resolution is True:
             self.command_options.append('-R')
             if self.dns_servers:
@@ -36,22 +47,21 @@ class NmapOptions:
         else:
             self.command_options.append('-n')
 
-    def _get_ports_option(self):
+    def _append_ports_option(self):
+        """Appends the ports option to the list of nmap options."""
         ports = f'-p {self.ports}' * (self.ports is not None)
         self.command_options.append(ports)
 
-    def _get_timing_option(self):
-        if self.timing_template > 5 or self.timing_template < 0:
-            logger.warning('The timing template should be between 0 and 5. The scan will use the default T3.')
-        else:
-            self.command_options.append('-T' + str(self.timing_template))
+    def _append_timing_option(self):
+        """Appends the timing template option to the list of nmap options."""
+        self.command_options.append(self.timing_template.value)
 
     def get_command_options(self) -> List[str]:
-        """Returns a list of nmap options."""
-        self._get_version_option()
-        self._get_dns_resolution_option()
-        self._get_ports_option()
-        self._get_timing_option()
+        """Returns the list of nmap options."""
+        self._append_enable_version_detection_option()
+        self._append_dns_resolution_option()
+        self._append_ports_option()
+        self._append_timing_option()
 
         return self.command_options
 
@@ -90,19 +100,17 @@ class NmapWrapper:
                    hosts_and_mask]
         return command
 
-    def _parse_output_to_json(self, xml_output: str) -> Dict[str, Any]:
-        """Parse the xml_output of the nmap scan command to json format.
+    def _parse_output(self, xml_output: str) -> Dict[str, Any]:
+        """Parse the xml_output of the nmap scan command.
 
         Args:
             xml_output: output of the nmap scan command.
 
         Returns:
-            json_output
+            dict of the scan's result.
         """
         parsed_xml = xmltodict.parse(xml_output)
-        json_output = json.dumps(parsed_xml, indent=4, sort_keys=True)
-        json_output = json.loads(json_output)
-        return json_output
+        return parsed_xml
 
     def scan(self, hosts: str, mask: str = '') -> Dict[str, Any]:
         """Run the scan with nmap.
@@ -118,5 +126,5 @@ class NmapWrapper:
         with subprocess.Popen(command, stdout=subprocess.PIPE) as process:
             xml_output = process.communicate()[0]
             xml_output = xml_output.decode(encoding='utf-8')
-            scan_results = self._parse_output_to_json(xml_output)
+            scan_results = self._parse_output(xml_output)
             return scan_results
