b7cef083dcaef5217dcbf48471e903209ce582ef
RabsonJ <phirirabsonj@gmail.com>
2022-03-29 13:09:43 +0000

    Fix pylint warnings

--- agent/markdown.py
+++ agent/markdown.py
@@ -10,13 +10,14 @@ def prepare_data_for_markdown_formatting(scans: Dict) -> List[List[str]]:
     Args:
         scans: Dictionary containing the scans, from the nmap scan response.
     Returns:
-        data: List of lists, each containing the name of the host, port, version, protocol, state, and service of its scan.
+        data: List of lists, each containing the name of the host, port, version, protocol, state,
+        and service of its scan.
     """
     scan_data = []
     if scans is not None:
-      for data in generators.get_services(scans):
-         row = [data['host'], data['port'], data['version'], data['protocol'], data['state'], data['service']]
-         scan_data.append(row)
+        for data in generators.get_services(scans):
+            row = [data['host'], data['port'], data['version'], data['protocol'], data['state'], data['service']]
+            scan_data.append(row)
     return scan_data
 
 
